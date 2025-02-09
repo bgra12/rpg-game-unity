3a644e30faedef6f700203b97133e782b2d9bc39
Bugra Kabiman <bugrakabiman@gmail.com>
2025-02-09 17:25:50 +0300

    player walk animations

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -9,14 +9,19 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float speed;
 
+    private readonly int moveX = Animator.StringToHash("MoveX");
+    private readonly int moveY = Animator.StringToHash("MoveY");
+    
     private PlayerActions actions;
     private Rigidbody2D rb2D;
     private Vector2 moveDirection;
+    private Animator animator;
     
     private void Awake()
     {
        actions = new PlayerActions();
        rb2D = GetComponent<Rigidbody2D>();
+       animator = GetComponent<Animator>();
     }
 
     // Start is called before the first frame update
@@ -44,6 +49,12 @@ public class PlayerMovement : MonoBehaviour
     private void readMovement()
     {
         moveDirection = actions.Movement.Move.ReadValue<Vector2>().normalized;
+        if (moveDirection == Vector2.zero)
+        {
+            return;
+        }
+        animator.SetFloat(moveX, moveDirection.x);
+        animator.SetFloat(moveY, moveDirection.y);
     }
     private void OnEnable()
     {
